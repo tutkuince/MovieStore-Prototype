f0900d409dd18148e616cb0d163a9327ad1ac3d9
tutkuince <tutku.ince@outlook.com>
2019-02-01 15:43:56 +0300

    init UI and addMovie()

--- WebContent/js/project.js
+++ WebContent/js/project.js
@@ -1,4 +1,32 @@
 const form = document.querySelector("#film-form");
-const title = document.getElementById("title");
-const director = document.getElementById("director");
-const url = document.getElementById("url");
+const titleElement = document.getElementById("title");
+const directorElement = document.getElementById("director");
+const urlElement = document.getElementById("url");
+
+// Init UI Object
+const ui = new UI();
+
+eventListeners();
+
+function eventListeners() {
+    form.addEventListener("submit", addMovie);
+}
+
+function addMovie(e) {
+    const title = titleElement.value;
+    const director = directorElement.value;
+    const url = urlElement.value;
+
+    if (title === "" || director === "" || url === "") {
+        // Error
+    } else {
+        // New movie
+        const newMovie = new Movie(title, director, url);
+
+        ui.addMovieToUI(newMovie);
+    }
+
+
+    // Prevent Submit
+    e.preventDefault();
+}
