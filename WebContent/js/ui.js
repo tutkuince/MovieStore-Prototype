419ec6a1c02eaa5ae1eca72d041a8b80f1e9421c
tutkuince <tutku.ince@outlook.com>
2019-02-01 16:00:38 +0300

    addMovieToUI method

--- WebContent/js/ui.js
+++ WebContent/js/ui.js
@@ -4,5 +4,28 @@ function UI() {
 }
 
 UI.prototype.addMovieToUI = function (newMovie) {
-    console.log(newMovie);
+// <tr>
+//     <td><img src="" class="img-fluid img-thumbnail"></td>
+//         <td></td>
+//         <td></td>
+//         <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+// </tr>
+
+    const movieList = document.getElementById("films");
+
+    movieList.innerHTML += `
+        <tr>
+            <td><img src="${newMovie.link}" class="img-fluid img-thumbnail"></td>
+            <td>${newMovie.title}</td>
+            <td>${newMovie.director}</td>
+            <td><a href="#" id = "delete-film" class = "btn btn-danger">Delete Movie</a></td>
+        </tr>
+    `;
+}
+
+UI.prototype.clearInputs = function (e1, e2, e3) {
+    e1.value = "";
+    e2.value = "";
+    e3.value = "";
+    e1.focus();
 }
